a32221925584ce01bd4d8311056a9c8bf5d593e0
diane-at-Okto <diane.kang@page-effect.com>
2022-08-09 17:48:30 +0200

    Clear index.php. All functions are now in Object named SinngrundKultureBank2

--- index.php
+++ index.php
@@ -13,59 +13,106 @@ if( ! defined( 'ABSPATH' ) ) exit; // Exit if accessed directly
 class SinngrundKultureBank {
   function __construct() {
     add_action('enqueue_block_editor_assets', array($this, 'adminAssets'));
+    add_action('add_meta_boxes', array($this, 'basic_info_boxes'));
+    add_action( 'save_post', array($this, 'save_basic_info_box' ));
   }
 
   function adminAssets() {
     wp_enqueue_script('sinngrund_kulture_bank_block_type', plugin_dir_url(__FILE__) . '/build/index.js', array('wp-blocks', 'wp-element', 'wp-block-editor'));
   }
+
+  /**
+  * Meta box display callback.
+  *
+  * @param WP_Post $post Current post object.
+  */
+  
+  
+  function basic_info_boxes_display_callback( $post ) {
+    include plugin_dir_path( __FILE__ ) . '/basic_info_box.php';
+    //include plugin_dir_path( __FILE__ ) . '/basic_info_box.php';
+    //require_once( get_plugin_directory() . "/basic_info_box.php" );
+  }
+  
+
+  function basic_info_boxes(){
+    add_meta_box(   'basic_info', // name
+                    __('Basic required data'), //display text 
+                    array($this, 'basic_info_boxes_display_callback'), // call back function  
+                    'post' );
+  }
+  
+
+  function save_basic_info_box( $post_id ) {
+    if ( defined( 'DOING_AUTOSAVE' ) && DOING_AUTOSAVE ) return;
+    if ( $parent_id = wp_is_post_revision( $post_id ) ) {
+        $post_id = $parent_id;
+    }
+    $fields = [
+        'latitude_longitude',
+        'latitude',
+        'longitude',
+        'popuptext'
+    ];
+    foreach ( $fields as $field ) {
+        if ( array_key_exists( $field, $_POST ) ) {
+            update_post_meta( $post_id, $field, sanitize_text_field( $_POST[$field] ) );
+        }
+     }
+  }
+  
+  
 }
 
 $sinngrundKultureBank = new SinngrundKultureBank();
 
 
-function slug_post_type_template() {
-	$page_type_object = get_post_type_object( 'post' );
-  $page_type_object->template = [
-		['sinngrund/kulture-datenbank' 
-    ],
-	];
-  //$post_type_object->template_lock = 'all'; // lock the template on the UI so that the blocks presented cannot be manipulated
-}
-add_action( 'init', 'slug_post_type_template' );
+// // This for post template (new post page) with  Blocks
+// function slug_post_type_template() {
+// 	$page_type_object = get_post_type_object( 'post' );
+//   $page_type_object->template = [
+// 		['sinngrund/kulture-datenbank' 
+//     ],
+// 	];
+//   //$post_type_object->template_lock = 'all'; // lock the template on the UI so that the blocks presented cannot be manipulated
+// }
+// add_action( 'init', 'slug_post_type_template' );
 
 
-function basic_info_boxes(){
-  add_meta_box(   'basic_info', // name
-                  __('Basic required data'), //display text 
-                  'basic_info_boxes_display_callback', // call back function  
-                  'post' );
-}
-add_action('add_meta_boxes', 'basic_info_boxes');
+// function basic_info_boxes(){
+//   add_meta_box(   'basic_info', // name
+//                   __('Basic required data'), //display text 
+//                   'basic_info_boxes_display_callback', // call back function  
+//                   'post' );
+// }
+// add_action('add_meta_boxes', 'basic_info_boxes');
 
-  /**
-* Meta box display callback.
-*
-* @param WP_Post $post Current post object.
-*/
-function basic_info_boxes_display_callback( $post ) {
-  include plugin_dir_path( __FILE__ ) . './basic_info_box.php';
-}
+//   /**
+// * Meta box display callback.
+// *
+// * @param WP_Post $post Current post object.
+// */
 
-function save_basic_info_box( $post_id ) {
-  if ( defined( 'DOING_AUTOSAVE' ) && DOING_AUTOSAVE ) return;
-  if ( $parent_id = wp_is_post_revision( $post_id ) ) {
-      $post_id = $parent_id;
-  }
-  $fields = [
-      'latitude_longitude',
-      'latitude',
-      'longitude',
-      'popuptext'
-  ];
-  foreach ( $fields as $field ) {
-      if ( array_key_exists( $field, $_POST ) ) {
-          update_post_meta( $post_id, $field, sanitize_text_field( $_POST[$field] ) );
-      }
-   }
-}
-add_action( 'save_post', 'save_basic_info_box' );
+
+// function basic_info_boxes_display_callback( $post ) {
+//   include plugin_dir_path( __FILE__ ) . '/basic_info_box.php';
+// }
+
+// function save_basic_info_box( $post_id ) {
+//   if ( defined( 'DOING_AUTOSAVE' ) && DOING_AUTOSAVE ) return;
+//   if ( $parent_id = wp_is_post_revision( $post_id ) ) {
+//       $post_id = $parent_id;
+//   }
+//   $fields = [
+//       'latitude_longitude',
+//       'latitude',
+//       'longitude',
+//       'popuptext'
+//   ];
+//   foreach ( $fields as $field ) {
+//       if ( array_key_exists( $field, $_POST ) ) {
+//           update_post_meta( $post_id, $field, sanitize_text_field( $_POST[$field] ) );
+//       }
+//    }
+// }
+// add_action( 'save_post', 'save_basic_info_box' );
