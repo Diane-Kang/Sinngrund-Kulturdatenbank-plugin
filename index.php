6a5da9ca55266b0d9731b1b1f2b694088888a383
diane-at-Okto <diane.kang@page-effect.com>
2022-08-12 18:16:48 +0200

    add meta data to REST API and shortcode

--- index.php
+++ index.php
@@ -23,26 +23,73 @@ class SinngrundKultureBank {
     add_action( 'admin_enqueue_scripts', array($this,'leaflet_dependency'), 10, 1 );
     // for Frontend dependency wp_enqueue_scripts
 
+    $target_page_name = 'sinngrund-kulturedatenbank';
 
+
+    add_action( 'wp_enqueue_scripts', array($this,'leaflet_dependency'), 10, 1 );
+    add_action( 'wp_enqueue_scripts', array($this, 'map_page_dependency'), 20, 1 );
+
+      
+    // This is for sinngrund-kulturedatenbank-diane
+    add_filter('page_template', array($this, 'loadTemplate'), 99);
+
+    //shortcode for beitrag list 
+    add_shortcode('show_list_shortcode', array($this, 'show_list_function'));
+
+    add_action('init', function(){
+
+      register_post_meta(
+          'post',
+          'latitude',
+          array(
+              'single'       => true,
+              'type'         => 'string',
+              'show_in_rest' => true,
+          )
+      );
+
+      register_post_meta(
+        'post',
+        'longitude',
+        array(
+            'single'       => true,
+            'type'         => 'string',
+            'show_in_rest' => true,
+        )
+    );
+  });
+  
+
+    register_rest_field( 'post', 'metadata', array(
+      'get_callback' => function ( $data ) {
+          return get_post_meta( $data['latitude'], '', '' );
+      }, ));
+  
+
+    //add_action( 'rest_api_init', 'geojson_generate_api');
+
+  
     //register_activation_hook(__FILE__, array($this, 'insert_main_map_page'));
     //register_deactivation_hook( __FILE__, array($this, 'deactivate_plugin'));
     //add_action('admin_head', array($this,'insert_main_map_page')
     //add_filter( 'page_template', array($this,'main_map_page_from_php') );
   
-    $target_page_name = 'sinngrund-kulturedatenbank';
-
-
-      add_action( 'wp_enqueue_scripts', array($this,'leaflet_dependency'), 10, 1 );
-      add_action( 'wp_enqueue_scripts', array($this, 'map_page_dependency'), 20, 1 );
-
-      add_filter('page_template', array($this, 'loadTemplate'), 99);
-
-      //add_shortcode('loadTemplate', 'kulturedatenbank_map_page_shortcode');
-
-
-
-  
   }
+
+
+    function wpse_382314_post_filter_data($response, $post) {
+        $response->data['post_title'] = '';
+        $response->data['post_content'] = '';
+    }
+
+
+  function show_list_function(){
+// Things that you want to do.
+$message = 'Hello world!'; 
+  
+// Output needs to be return
+return $message;
+}
 
   function leaflet_dependency(){
     wp_enqueue_style( 'leaflet-main-css',                   plugin_dir_url( __FILE__ ) . '/node_modules/leaflet/dist/leaflet.css' , array(), false, false);
@@ -52,24 +99,20 @@ class SinngrundKultureBank {
   function map_page_dependency(){
     if (is_page($target_page_name)){
       wp_enqueue_script( 'map_modify-js',                     plugin_dir_url( __FILE__ ) . '/map_modify.js', array('leaflet-js'), '1.3', true);
-      
+    
       // map-app-style.css, controled by .page-id-227!!!!!
       wp_enqueue_style( 'map-app-style-css', plugin_dir_url( __FILE__ ) . '/map-app-style.css', array(), '3.2', false);
     }
   }
   
+  // Gutenberg, block javascript 
   function adminAssets() {
     wp_enqueue_script('sinngrund_kulture_bank_block_type', plugin_dir_url(__FILE__) . '/build/index.js', array('wp-blocks', 'wp-element', 'wp-block-editor'));
     
   }
 
 
-  
-
-  // function load_kulturedatenbank_map_page(){
-  //   add_filter('template_include', array($this, 'loadTemplate'), 99);
-  // }
-
+  // This is for sinngrund-kulturedatenbank-diane
   function loadTemplate($template) {
     if (is_page('sinngrund-kulturedatenbank-diane')) {
       return plugin_dir_path(__FILE__) . 'main_map_page_diane.php';
@@ -104,10 +147,8 @@ class SinngrundKultureBank {
         $post_id = $parent_id;
     }
     $fields = [
-        'latitude_longitude',
         'latitude',
-        'longitude',
-        'popuptext'
+        'longitude'
     ];
     foreach ( $fields as $field ) {
         if ( array_key_exists( $field, $_POST ) ) {
@@ -115,47 +156,11 @@ class SinngrundKultureBank {
         }
      }
   }
-
   
 
-  function insert_main_map_page() {
-    $check_map_page = $this->get_page_by_slug('sinngrund_Kulturedatenbank'); 
-    echo $check_map_page;
-      if (!$check_map_page){
-        $main_map_page = array(
-          'post_type' => 'page',
-          'post_name' => 'sinngrund_Kulturedatenbank',
-          'post_title' => 'Sinngrund Kulturedatenbank',
-          'post_status' => 'publish',
-        );
-
-        wp_insert_post($main_map_page);
-      }
-    }
-  
-  function get_page_by_slug($slug) {
-      if ($pages = get_pages())
-          foreach ($pages as $page)
-              if ($slug === $page->post_name) return $page;
-      return false;
-  } // function get_page_by_slug
 
 
 
-
-  function main_map_page_from_php( $page_template ){
-      if ( is_page( 'Sinngrund_Kulturedatenbank' ) ) {
-
-          $page_template = plugin_dir_path( __FILE__ ) . '/kulturedatenbank.php';
-      }
-      return $page_template;
-  }
-  
-  function deactivate_plugin() {
-    $page = get_page_by_path('sinngrund_Kulturedatenbank', '', 'page' );
-    wp_delete_post($page->ID);
-  }
-  
 }
 
 $sinngrundKultureBank = new SinngrundKultureBank();
