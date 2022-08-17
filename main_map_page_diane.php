f609bfa60e642c68e7afc680b394530334480082
diane-at-Okto <diane.kang@page-effect.com>
2022-08-17 10:12:37 +0200

    map page modified

--- main_map_page_diane.php
+++ main_map_page_diane.php
@@ -20,10 +20,74 @@ Example : I have placed welcome.php file in the /var/www/html/welcome.php then u
 <link rel="stylesheet" href="<?php echo plugin_dir_url( __FILE__ ) . '/node_modules/leaflet/dist/leaflet.css'?>" />
 <style type="text/css">
 html, body { width:100%;padding:0;margin:0; }
+
 #map { width:70%;height:100%;padding:0;margin:0; float:left;}
-#side_bar { width:25%;height:100%;padding:20px;margin:0; }
+
+#side_bar { width:27%;height:100%;padding-left:20px;margin:0; }
+
 .main_block{display:inline-block;}
+
+.search {
+  width: 100%;
+  position: relative;
+  display: flex;
+}
+
+.searchTerm {
+  width: 100%;
+  border: 3px solid #00B4CC;
+  border-right: none;
+  padding: 5px;
+  height: 36px;
+  border-radius: 5px 0 0 5px;
+  outline: none;
+  color: #9DBFAF;
+}
+
+.searchTerm:focus{
+  color: #00B4CC;
+}
+
+.searchButton {
+  width: 40px;
+  height: 36px;
+  border: 1px solid #00B4CC;
+  background: #00B4CC;
+  text-align: center;
+  color: black;
+  border-radius: 0 5px 5px 0;
+  cursor: pointer;
+  font-size: 20px;
+}
+
+
+.datenbank_list{
+	height: 60vh;
+	overflow-y: scroll;
+}
+
+.datenbank_single_entry{
+	
+  background-color:  #ccebff ;
+
+  padding-left: 10px;
+  padding-bottom: 10px;
+  margin-top: 0px;
+  padding-top: 5px;
+  border-top-width: 1px;
+  border-top-style: solid;
+  border-left-width: 1px;
+  border-left-style: solid;
+  border-bottom-width: 1px;
+  border-bottom-style: solid;
+  border-right-width: 1px;
+  border-right-style: solid;
+  padding-right: 10px;
+}
+
+
 .container { width:95%;max-width:980px;padding:1% 2%;margin:0 auto }
+
 #lat, #lon { text-align:right }
 .address { cursor:pointer }
 .address:hover { color:#AA0000;text-decoration:underline }
@@ -31,6 +95,17 @@ html, body { width:100%;padding:0;margin:0; }
 </head>
 <body>
 	<div calss="main_block" id="map"></div>
+	<div calss="main_block" id="entry_display" style="display:none;">
+		<h3>Post title</h3>
+		<p>category author datum location</p>
+		<div class="entry_content"> </div>
+		<p> Lorem ipsum dolor sit amet, consectetur adipiscing elit. Proin non purus non elit convallis porttitor. Nulla auctor augue vel augue pretium fermentum at at orci. Aliquam sagittis suscipit justo a eleifend. Vestibulum nec lectus felis. Suspendisse eget arcu leo. Pellentesque in mauris facilisis, rutrum nisl at, luctus odio. Mauris euismod odio sit amet aliquet pharetra. Nullam eu cursus dui. Nam et nisl gravida purus laoreet blandit non in est. Donec in est vel urna commodo fringilla a ac libero. Duis tristique purus non laoreet tincidunt. Vivamus commodo dolor non ipsum bibendum ultrices. Donec posuere nibh ac volutpat vehicula. Proin nec tellus consequat, venenatis felis id, facilisis dolor. Sed rutrum tortor eget nulla varius tristique. Suspendisse non tortor tincidunt, cursus ex non, ultrices felis.
+
+		Donec tempor sed lacus vel molestie. Nunc id orci vel nunc rutrum tempor. Nunc augue tortor, scelerisque ac commodo id, interdum vel metus. Nunc euismod ligula neque, sit amet tincidunt purus venenatis eu. Donec vitae volutpat risus, eu efficitur massa. Curabitur non ullamcorper ex. Morbi nec sodales lorem, quis consequat nibh. In id sapien at velit congue suscipit. Mauris id sodales nisi, in fringilla ligula. Fusce eget interdum elit. Fusce est mauris, fermentum ut volutpat sit amet, accumsan at risus. Vivamus at nibh ipsum.
+
+		</p>
+
+	</div>
 	<div class="main_block" id="side_bar">
 
 		<h2>Post title</h2>
@@ -51,11 +126,30 @@ html, body { width:100%;padding:0;margin:0; }
 
 
 		foreach ($category_icon_array as $name => $icon)  {
-            echo '<li><input type="checkbox">'. $name . '<br />';
+			$category_icon = $category_icon_array[$name];
+			$category_icon_src = '/wp-content/plug	ins/Sinngrund-Kulturdatenbank-plugin/icons/'. $category_icon;
+            echo '<input type="checkbox"><img style="height: 20px; width: 20px; margin-right: 2px;"  src="'.$category_icon_src.'"/>'. $name . '<br />';
         }
 		
 		?>
-	</div> <!-- closing div id checkboxes  -->
+		</div> <!-- closing div id checkboxes  -->
+
+		<div class="sort_options">
+			such nach 
+			<select  name="uhrzeit" id="abschaltung_uhrzeit">
+			<option value="0" selected >Aktuellst zuerst</option>
+			<option value="1" selected >Title:Alpabet?</option>
+			<option value="2" selected >Author:Alphabet</option>
+			</select>
+		</div>
+
+
+		<div class="search">
+			<input type="text" class="searchTerm" placeholder="What are you looking for?">
+			<button type="submit" class="searchButton">
+			<svg viewBox="0 0 1024 1024"><path class="path1" d="M848.471 928l-263.059-263.059c-48.941 36.706-110.118 55.059-177.412 55.059-171.294 0-312-140.706-312-312s140.706-312 312-312c171.294 0 312 140.706 312 312 0 67.294-24.471 128.471-55.059 177.412l263.059 263.059-79.529 79.529zM189.623 408.078c0 121.364 97.091 218.455 218.455 218.455s218.455-97.091 218.455-218.455c0-121.364-103.159-218.455-218.455-218.455-121.364 0-218.455 97.091-218.455 218.455z"></path></svg>
+			</button>
+		</div>
 
 
 		<?php 
@@ -73,9 +167,10 @@ html, body { width:100%;padding:0;margin:0; }
 			$category_icon = $category_icon_array[$category_name];
 			$category_icon_src = '/wp-content/plugins/Sinngrund-Kulturdatenbank-plugin/icons/'. $category_icon;
 			$url = '/wp-content/plugins/Sinngrund-Kulturdatenbank-plugin/icons/star.png';
-			$string .=' <div class="datenbank_single_entry">
+			$string .=' <div class="datenbank_single_entry" id="post_'. get_the_ID() .'">
 						  <div class="entry_title"><h4> this is post title:' . get_the_title() .'</h4></div>
-						  <div class="entry_category ' .$category_icon. '"><img style="height: 20px; width: 20px; margin-right: 2px;"  src="'.$category_icon_src.'"/>'.$category_name.'</div>
+						  <div class="entry_category' .$category_icon. '"><img style="height: 20px; width: 20px; margin-right: 2px;"  src="'.$category_icon_src.'"/>'.$category_name.'</div>
+						  <div class="eingrag_ansehen_button" id="button_to_post_'. get_the_ID() .'" >Eingrag ansehen</div>
 						</div>'; //closing class datenbank_single_entry
 		
 		  }
@@ -92,50 +187,7 @@ html, body { width:100%;padding:0;margin:0; }
 		?>
 
 	</div>
-	
 
-	<div class="container">
-
-		<div class="manual_input">
-			<p class="meta-options1 basic_info_field">
-				<label for="latitude">Breitengrad(latitude)</label>
-				<input  id="latitude" type="text" name="latitude"
-						value="<?php echo "hello" ?>"
-				>
-			</p>
-			<p class="meta-options2 basic_info_field">
-				<label for="longitude">Längengrad(longitude)</label>
-				<input id="longitude" type="text" name="longitude"
-				value="<?php echo "hello" ?>"
-				>
-			</p>
-			<p class="meta-options3 basic_info_field">
-				<label for="popuptext">text for marker</label>
-				<input id="popuptext" type="text" name="popuptext"
-				value="<?php echo "hello" ?>"
-						>
-			</p>
-		</div>
-
-		<b>Coordinates</b>
-		<form>
-		Lat <input type="text" name="lat" id="lat" size=12 value="">
-		Lon <input type="text" name="lon" id="lon" size=12 value="">
-		<button type="button" onclick="save_geocode_metadata();">use it</button>
-		</form>
-
-		<b>Address Lookup</b>
-		<div id="search">
-		<input type="text" name="addr" value="" id="addr" size="58" />
-		<button type="button" onclick="addr_search();">Search</button>
-		<div id="results"></div>
-		</div>
-
-		<br />
-
-		
-
-	</div>
 
 <script type="text/javascript">
 
@@ -162,9 +214,9 @@ async function main() {
 	var options = {
  	center: info_json.map_center,
 	zoomSnap: 0.1,
- 	zoom: 12.5
+ 	zoom: 12.5,
+	zoomControl: false
 	}
-
 
 	const map = L.map('map', options);
 
@@ -173,6 +225,10 @@ async function main() {
         minZoom: 5,
         attribution: 'Map data and Imagery &copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors',
     }).addTo(map);
+
+	L.control.zoom({
+    	position: 'bottomright'
+	}).addTo(map);
 
     //php directory url, start with ".", javascript start with nothing
     var icons_loc = info_json.icons_directory.replace(".", "");
