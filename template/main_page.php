d35d0982bb6d23c9d3a1db7b62b065af1bfc7159
Thomas Pondelek <thomas.pondelek@page-effect.com>
2022-09-19 12:57:05 +0200

    css change and html markup change main page

--- template/main_page.php
+++ template/main_page.php
@@ -2,25 +2,27 @@
 <?php include 'head.php'; ?>
 
 <body>
-  <div calss="main_block main_page_left">
+  <div class="main_block main_page_left">
 
     <div class="main_map_block" id="main_page_map"></div>
-    <div class="map_top_banner" id="map_top_banner" style="width:70%; border: 1px solid red; z-index: 999; position:absolute;
+    <!-- <div class="map_top_banner" id="map_top_banner" style="width:70%; border: 1px solid red; z-index: 999; position:absolute;
 		top:0; left:0;">
-      map top banner</div>
-    <div class="main_block main_page_right" id="side_bar">
+      map top banner</div> -->
+    <div class="main_block main_page_right sidebar" id="side_bar">
 
-      <div id="checkboxes">
+      <div id="checkboxes" class="category_filter_section">
         <!--div id checkboxes  -->
-        <p>Kategory</p>
+        <h2 class="category">Kategorie</h2>
         <?php  
 			$category_shortname_array  = $sinngrundKultureBank->get_category_shortname_array();
 
 			foreach ($category_shortname_array as $name => $shortname)  {
 				$category_shortname = $category_shortname_array[$name];
 				$category_icon = $category_shortname_array[$name].'.svg';
-				$category_icon_src = '/wp-content/plug	ins/Sinngrund-Kulturdatenbank-plugin/icons/'. $category_icon;
-							echo '<input type="checkbox" value="'.$category_shortname.'" category_name="'.$name .'" name="kategory_filter" checked="true"><img style="height: 20px; width: 20px; margin-right: 2px;"  src="'.$category_icon_src.'"/>'. $name . '<br />';
+				$category_icon_src = '/wp-content/plugins/Sinngrund-Kulturdatenbank-plugin/icons/'. $category_icon;
+							echo '<div class="single_category_wrapper"><input class="cat_checkbox" type="checkbox" id="'.$category_shortname.'" value="'.$category_shortname.'" category_name="'
+              .$name .'" name="kategory_filter" checked="true">
+              <img class="cat_icon" style="height: 20px; width: 20px; margin-right: 2px;"  src="'.$category_icon_src.'"/><label class="cat_label" for="'.$category_shortname.'">'. $name .'</label></div>';
 					}
 			
 			?>
