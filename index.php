5130d987e222845bc823f2adc172793189c0aa9f
diane-at-Okto <diane.kang@page-effect.com>
2022-09-27 16:25:27 +0200

    change a version number

--- index.php
+++ index.php
@@ -2,8 +2,8 @@
 
 /*
   Plugin Name: Sinngrund kulturebank plugin 
-  Description: Es ist für Sinngrund kulturebank project: last updated at 12.Sep 14:00
-  Version: 2.5
+  Description: Es ist für Sinngrund kulturebank project: last updated at 27.Sep 16:00
+  Version: 2.6 
   Author: Page-effect 
   Author-email: Diane.kang@page-effect.com
 
